7420f47fa5a3ff123ebf936ffa69e1b9e5f6f217
rafli.haidar30@gmail.com <rafli.haidar30@gmail.com>
2023-07-02 21:26:05 +0700

    add footer page

--- src/page/FooterPage.vue
+++ src/page/FooterPage.vue
@@ -4,47 +4,48 @@
             <div>
                 <p class="text-2xl text-white font-bold">Toko</p>
                 <p class="font-bold text-white">Berdasarkan Kategori</p>
-                <ul>
-                    <li>Jajanan Pasar</li>
-                    <li>Pakaian</li>
-                    <li>Makanan</li>
-                    <li>Mainan</li>
-                    <li>Transportasi</li>
-                    <li>E-warteg</li>
+                <ul class="font-semibold cursor-pointer">
+                    <li class="hover:text-white transition-colors">Jajanan Pasar</li>
+                    <li class="hover:text-white transition-colors">Pakaian</li>
+                    <li class="hover:text-white transition-colors">Makanan</li>
+                    <li class="hover:text-white transition-colors">Mainan</li>
+                    <li class="hover:text-white transition-colors">Transportasi</li>
+                    <li class="hover:text-white transition-colors">E-warteg</li>
                 </ul>
             </div>
             <div>
                 <p class="text-2xl font-bold text-white">Dukungan</p>
-                <ul>
-                    <li>Pencari Toko</li>
-                    <li>Status Pesanan</li>
+                <ul class="font-semibold cursor-pointer">
+                    <li class="hover:text-white transition-colors">Pencari Toko</li>
+                    <li class="hover:text-white transition-colors">Status Pesanan</li>
                 </ul>
             </div>
             <div>
                 <p class="text-2xl font-bold text-white">Perusahaan</p>
-                <ul>
-                    <li>Pelayanan Pelanggan</li>
-                    <li>Syarat Penggunaan</li>
-                    <li>Privasi</li>
-                    <li>Karir</li>
-                    <li>Tentang</li>
-                    <li>Keberlanjutan</li>
-                    <li>Afiliasi</li>
+                <ul class="font-semibold cursor-pointer">
+                    <li class="hover:text-white transition-colors">Pelayanan Pelanggan</li>
+                    <li class="hover:text-white transition-colors">Syarat Penggunaan</li>
+                    <li class="hover:text-white transition-colors">Privasi</li>
+                    <li class="hover:text-white transition-colors">Karir</li>
+                    <li class="hover:text-white transition-colors">Tentang</li>
+                    <li class="hover:text-white transition-colors">Keberlanjutan</li>
+                    <li class="hover:text-white transition-colors">Afiliasi</li>
                 </ul>
             </div>
-            <div class="grid grid-cols-1 gap-y-5">
+            <div class="grid grid-cols-1 gap-y-5 font-semibold cursor-pointer">
                 <p class="text-2xl font-bold text-white">Kontak</p>
                 <div>
                     <p class="text-white font-bold">Email</p>
-                    <p>Kontak kami</p>
+                    <p class="hover:text-white transition-colors">Kontak kami</p>
                 </div>
                 <div>
                     <p class="text-white font-bold">Telepon</p>
-                    <p>855-909-8257</p>
+                    <p class="hover:text-white transition-colors">855-909-8257</p>
                 </div>
                 <div>
                     <p class="text-white font-bold">Address</p>
-                    <p>Jl. Raya Kalirungkut No.2, Kali Rungkut, Kec. Rungkut, Surabaya, Jawa Timur 60293</p>
+                    <p class="hover:text-white transition-colors">Jl. Raya Kalirungkut No.2, Kali Rungkut, Kec. Rungkut,
+                        Surabaya, Jawa Timur 60293</p>
                 </div>
             </div>
         </div>
